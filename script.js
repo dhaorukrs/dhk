d46337d223189d56e25731aed53ea5eb7ba7a6e9
dhaorukrs <dhaorukrs@gmail.com>
2024-10-28 00:14:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     } else if (username) {
         const headerElement = document.querySelector('header h3');
         if (headerElement) {
-            headerElement.textContent = `Hi - ${username}`;
+            headerElement.textContent = `${username}`;
         }
     }
 });
